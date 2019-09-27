609ae4032dd6dce5240e63dca1ccaa1acb5c1a4e
Vorpalbunnie <31486016+Vorpalbunnie@users.noreply.github.com>
2019-09-27 15:17:49 -0400

    dddd

--- testing.js
+++ testing.js
@@ -1,2 +1,5 @@
-window.onload = console.log("success")
-document.onload = console.log("docLoaded")
+window.onload = function () {
+// do the work after everything was loaded (DOM, media elements)
+  console.log("success")
+}
+  document.onload = function () {console.log("docLoaded")}
